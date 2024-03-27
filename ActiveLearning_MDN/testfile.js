af5deb18786d6899c9c1a7ac18fc5667a852145b
TahjayW <tahjay.watson@gmail.com>
2024-03-26 22:11:40 -0400

    Added testfile.js
    shopping list seems to be locked, troubleshooting
    version control

---
+++ ActiveLearning_MDN/testfile.js
@@ -0,0 +1,35 @@
+//Query selections for items
+const list = document.querySelectorAll("ul");
+
+const input = document.querySelectorAll("input");
+
+const buttons = document.querySelectorAll("button");
+
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        aFunction(input,list);
+    });
+});
+
+
+
+
+
+function aFunction(input,list) {
+    //Get input from user
+    const thisValue = input.textContent;
+    input.textContent = "";
+    //Create three buttons
+    const listItem = document.createElement("li");
+    const aSpan = document.createElement("span");
+    const anotherButton = document.createElement("button");
+    //Add Button to 
+    listItem.append(aSpan, anotherButton);
+
+    aSpan.textContent = thisValue;
+    anotherButton.textContent = "delete";
+    list.append(listItem);
+
+    anotherButton.addEventListener("click", list.removeChild(listItem));
+}
